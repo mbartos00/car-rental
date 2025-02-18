b0027ea48db0c9bd4d57f9abc0588d72332e8bea
mbartos00 <maciekbartos8@gmail.com>
2025-02-18 16:19:30 +0100

    feat: add user types

--- server/src/shared/types.ts
+++ server/src/shared/types.ts
@@ -1,3 +1,9 @@
 import { User } from '@prisma/client';
+import { z } from 'zod';
+import { updateUserSchema, userSchema } from './schemas/user.schema';
 
 export type UserWithoutPassword = Omit<User, 'password'>;
+
+export type ValidUser = z.infer<typeof userSchema>;
+
+export type ValidUpdatedUser = z.infer<typeof updateUserSchema>;
