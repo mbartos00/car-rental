44573e9c20f16b5783a0db803e646c06181aaebd
mbartos00 <maciekbartos8@gmail.com>
2025-02-18 16:19:57 +0100

    feat: add zod filter, set global prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ZodFilter } from './shared/zod-filter/zod.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new ZodFilter());
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 
