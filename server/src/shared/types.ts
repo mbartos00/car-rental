ab940b6476d7e878c8ce4ae4ea1dc9b51d6f14fe
mbartos00 <maciekbartos8@gmail.com>
2025-06-09 15:16:20 +0200

    feat: add new types

--- server/src/shared/types.ts
+++ server/src/shared/types.ts
@@ -1,9 +1,32 @@
 import { User } from '@prisma/client';
 import { z } from 'zod';
-import { updateUserSchema, userSchema } from './schemas/user.schema';
+import {
+  loginSchema,
+  updateUserSchema,
+  userSchema,
+} from './schemas/user.schema';
+
+declare module 'express' {
+  interface Request {
+    cookies: { [key: string]: string };
+  }
+}
 
 export type UserWithoutPassword = Omit<User, 'password'>;
 
 export type ValidUser = z.infer<typeof userSchema>;
-
+export type LoginInput = z.infer<typeof loginSchema>;
 export type ValidUpdatedUser = z.infer<typeof updateUserSchema>;
+
+export type JwtPayload = {
+  sub: string;
+  email: string;
+  role: string;
+  iat?: number;
+  exp?: number;
+};
+
+export type AuthTokens = {
+  accessToken: string;
+  refreshToken: string;
+};
