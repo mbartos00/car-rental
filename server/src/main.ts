a6668e94dc5d5ebe1161540501cda52a4ba839fd
mbartos00 <maciekbartos8@gmail.com>
2025-06-09 15:11:55 +0200

    feat: add cookie parser

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ZodFilter } from './shared/zod-filter/zod.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new ZodFilter());
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
 
