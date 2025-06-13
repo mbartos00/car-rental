b7315a75496fe97afa495faab1794747c8f17bc4
mbartos00 <maciekbartos8@gmail.com>
2025-06-13 14:12:07 +0200

    feat: add auth module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './db/prisma.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    PrismaModule,
-    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'uploads') }),
-  ],
+  imports: [AuthModule, UsersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
