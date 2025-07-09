e2f3ba88cca0292fbd31d14aea4fb434af6067b3
mbartos00 <maciekbartos8@gmail.com>
2025-07-09 13:26:12 +0200

    feat: add reviews schema

---
+++ server/src/shared/schemas/reviews.schema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const reviewSchema = z.object({
+  description: z.string().min(10),
+  rating: z.number().positive().min(1).max(5),
+  carId: z.string().regex(/^[a-f\d]{24}$/i, { message: 'Invalid ObjectId' }),
+});
+
+export const updateReviewSchema = z.object({
+  description: z.string().min(10).optional(),
+  rating: z.number().positive().min(1).max(5).optional(),
+  carId: z.string().regex(/^[a-f\d]{24}$/i, { message: 'Invalid ObjectId' }),
+});
