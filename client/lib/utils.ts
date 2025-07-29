c96098e220d00f9a57ae6e4d3c0afc60c08abf43
mbartos00 <maciekbartos8@gmail.com>
2025-07-29 13:15:19 +0200

    (client) feat: add new util

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -6,5 +6,17 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const toTitleCase = (str: string) => {
-  return str.substring(0, 1).toUpperCase() + str.substring(1, str.length);
+  return (
+    str.substring(0, 1).toUpperCase() +
+    str.substring(1, str.length).toLowerCase()
+  );
+};
+
+export const formatPriceToUSD = (price: number, fractionDigits = 2) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: fractionDigits,
+  }).format(price || 0);
 };
