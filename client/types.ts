cc7c339f9cc3070074e48cf57a90cdbf02c63187
mbartos00 <maciekbartos8@gmail.com>
2025-07-29 13:15:55 +0200

    (client) feat: add types file

---
+++ client/types.ts
@@ -0,0 +1,59 @@
+export type Car = {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  carType: CarType;
+  images: string[];
+  tankCapacity: number;
+  gearbox: Gearbox;
+  seats: number;
+  reviews: Review[];
+  favouritesListId?: string;
+  createdAt: string;
+};
+
+export type CarType = "SEDAN" | "SUV" | "CONVERTIBLE" | "COUPE" | "HATCHBACK";
+
+export type Gearbox = "MANUAL" | "AUTOMATIC";
+
+export type Review = {
+  id: string;
+  description: string;
+  rating: number;
+  carId: string;
+  createdAt: string;
+};
+
+export type CarFilters = {
+  price: {
+    min: number;
+    max: number;
+  };
+  tankCapacity: {
+    min: number;
+    max: number;
+  };
+  carType: {
+    _count: number;
+    carType: CarType;
+  }[];
+
+  gearbox: {
+    _count: number;
+    gearbox: Gearbox;
+  }[];
+  seats: {
+    _count: number;
+    seats: number;
+  }[];
+};
+
+export type Pagination = {
+  page: number;
+  limit: number;
+  total: number;
+  totalPages: number;
+  hasNext: boolean;
+  hasPrev: boolean;
+};
