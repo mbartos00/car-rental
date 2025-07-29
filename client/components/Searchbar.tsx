534b8416af60628f765c44cc3357f8051bc19b3b
mbartos00 <maciekbartos8@gmail.com>
2025-07-29 13:18:58 +0200

    (client) fix: control value based on query params

--- client/components/Searchbar.tsx
+++ client/components/Searchbar.tsx
@@ -1,20 +1,31 @@
+"use client";
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
 import { cn } from "@/lib/utils";
 import Form from "next/form";
+import { useSearchParams } from "next/navigation";
 
 type Props = {
   className?: string;
 };
 
 const Searchbar = ({ className }: Props) => {
+  const searchParams = useSearchParams();
+
+  const searchValue = searchParams.get("search") || "";
+
   return (
-    <Form action="/cars" className={cn("relative", className)}>
+    <Form
+      key={searchParams.get("search")}
+      action="/cars"
+      className={cn("relative", className)}
+    >
       <Input
         className={
           "border-secondary-200/40 pl-10 py-6 placeholder:text-sm placeholder:text-secondary-400 placeholder:font-medium lg:rounded-4xl lg:py-5"
         }
         name="search"
+        defaultValue={searchValue}
         placeholder="Search something here"
       />
       <Search className="absolute top-1/2 -translate-y-1/2 left-3 stroke-secondary-400" />
