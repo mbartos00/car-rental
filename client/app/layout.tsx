6f0d83909b58795e01d9d576c5f8de1d438dc8ed
mbartos00 <maciekbartos8@gmail.com>
2025-08-04 12:15:07 +0200

    (client) feat: add toaster

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,6 +3,7 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Toaster } from "sonner";
 
 const plusJakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -28,6 +29,16 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Toaster
+          toastOptions={{
+            classNames: {
+              title: "!text-secondary-500",
+              success: "!bg-primary-0",
+            },
+          }}
+          duration={3000}
+          richColors
+        />
       </body>
     </html>
   );
