2b4d54e70ff0099292a1dcb4c84eb582e59fc42d
mbartos00 <maciekbartos8@gmail.com>
2025-08-04 14:00:24 +0200

    (client) feat: add register form

---
+++ client/components/RegisterForm.tsx
@@ -0,0 +1,104 @@
+"use client";
+import { registerFormAction } from "@/api/actions";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { RegisterState } from "@/types";
+import Form from "next/form";
+import Link from "next/link";
+import { useActionState } from "react";
+import FormFieldInput from "./FormFieldInput";
+
+const initialState: RegisterState = {
+  errors: {
+    firstName: undefined,
+    lastName: undefined,
+    email: undefined,
+    password: undefined,
+    repeatPassword: undefined,
+  },
+  success: undefined,
+};
+
+const RegisterForm = () => {
+  const [state, formAction, pending] = useActionState(
+    registerFormAction,
+    initialState
+  );
+
+  return (
+    <>
+      <Card className="w-full max-w-md">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-secondary-500 text-2xl font-bold text-center">
+            Create Account
+          </CardTitle>
+          <CardDescription className="text-center text-secondary-400">
+            Enter your information to create your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form action={formAction} className="space-y-4">
+            <FormFieldInput
+              label="First Name"
+              fieldName="firstName"
+              placeholder="John"
+              required
+              errors={state.errors?.firstName}
+            />
+            <FormFieldInput
+              label="Last Name"
+              fieldName="lastName"
+              placeholder="Doe"
+              required
+              errors={state.errors?.lastName}
+            />
+            <FormFieldInput
+              label="Email"
+              fieldName="email"
+              placeholder="john.doe@example.com"
+              required
+              type="email"
+              errors={state.errors?.email}
+            />
+            <FormFieldInput
+              label="Password"
+              fieldName="password"
+              placeholder="Enter your password"
+              required
+              type="password"
+              errors={state.errors?.password}
+            />
+            <FormFieldInput
+              label="Repeat password"
+              fieldName="repeatPassword"
+              placeholder="Repeat password"
+              required
+              type="password"
+              errors={state.errors?.repeatPassword}
+            />
+            <Button type="submit" className="w-full" disabled={pending}>
+              {pending ? "Creating Account..." : "Create Account"}
+            </Button>
+          </Form>
+        </CardContent>
+        <CardFooter className="flex flex-col space-y-4">
+          <div className="text-center text-sm text-gray-600">
+            {"Already have an account? "}
+            <Button variant="link" asChild className="p-0 text-primary-500">
+              <Link href="/login">Log in</Link>
+            </Button>
+          </div>
+        </CardFooter>
+      </Card>
+    </>
+  );
+};
+
+export default RegisterForm;
