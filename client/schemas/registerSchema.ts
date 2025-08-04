fdb1d427f55e020d2a1164cbfa8539bd8ae8d914
mbartos00 <maciekbartos8@gmail.com>
2025-08-04 14:00:39 +0200

    (client) feat: add register schema

---
+++ client/schemas/registerSchema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { passwordSchema } from "./loginSchema";
+
+export const registerSchema = z
+  .object({
+    firstName: z
+      .string()
+      .min(2, "First name should have at least 2 characters"),
+    lastName: z.string().min(2, "Last name should have at least 2 characters"),
+    email: z.email("Invalid email"),
+    password: passwordSchema,
+    repeatPassword: z.string(),
+  })
+  .superRefine(({ repeatPassword, password }, ctx) => {
+    if (repeatPassword !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "Passwords did not match",
+        path: ["repeatPassword"],
+      });
+    }
+  });
