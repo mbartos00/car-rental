0ba471be494e2ccb07f7fb97761cb29e56f12121
mbartos00 <maciekbartos8@gmail.com>
2025-08-21 14:21:27 +0200

    (server) feat: limit description char count

--- server/src/shared/schemas/reviews.schema.ts
+++ server/src/shared/schemas/reviews.schema.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod';
 
 export const reviewSchema = z.object({
-  description: z.string().min(10),
+  description: z.string().min(10).max(150),
   rating: z.number().positive().min(1).max(5),
   carId: z.string().regex(/^[a-f\d]{24}$/i, { message: 'Invalid ObjectId' }),
 });
 
 export const updateReviewSchema = z.object({
-  description: z.string().min(10).optional(),
+  description: z.string().min(10).max(150).optional(),
   rating: z.number().positive().min(1).max(5).optional(),
   carId: z.string().regex(/^[a-f\d]{24}$/i, { message: 'Invalid ObjectId' }),
 });
