8a42eeb27479df6516f410aa818fc124f6b00cb3
mbartos00 <maciekbartos8@gmail.com>
2025-08-21 14:26:54 +0200

    (client) feat: add car details page

---
+++ client/app/cars/[id]/page.tsx
@@ -0,0 +1,36 @@
+import { getCar } from "@/api/api";
+import CarDescription from "@/components/CarDescription";
+import CarDetailsCarousel from "@/components/CarDetailsCarousel";
+import CarReviews from "@/components/CarReviews";
+import PopularCars from "@/components/PopularCars";
+
+export default async function Car({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const car = await getCar(id);
+
+  return (
+    <section className="flex flex-col gap-8 py-8 px-6 2xl:w-4/5 2xl:mx-auto">
+      <div className="flex flex-col gap-8 md:flex-row md:max-w-[1280px] md:mx-auto ">
+        <CarDetailsCarousel images={car.images} />
+        <CarDescription
+          id={car.id}
+          carType={car.carType}
+          description={car.description}
+          gearbox={car.gearbox}
+          price={car.price}
+          name={car.name}
+          tankCapacity={car.tankCapacity}
+          seats={car.seats}
+          reviewCount={car.reviewCount}
+          averageReview={car.averageReview}
+        />
+      </div>
+      <CarReviews reviews={car.reviews} reviewCount={car.reviewCount} />
+      <PopularCars wrapperClassName="lg:px-0" />
+    </section>
+  );
+}
