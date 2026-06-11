b9f3b09faca975e85a21fdc1809fd443c6f0e64b
mbartos00 <maciekbartos8@gmail.com>
2026-06-11 15:00:00 +0200

    (client) refactor: move customer pages into (main) route group

--- client/app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import RegisterForm from "@/components/RegisterForm";
-
-export default function Register() {
-  return (
-    <div className="flex min-h-[80svh] items-center justify-center p-4 lg:p-8">
-      <RegisterForm />
-    </div>
-  );
-}
